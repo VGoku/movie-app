f3d01abe8ab97587f65f0264f94d9d28651c6bd9
VGoku <neojohn2312@gmail.com>
2025-02-03 19:24:48 -0600

    push3

--- vite.config.js
+++ vite.config.js
@@ -1,22 +1,18 @@
 import { defineConfig } from 'vite';
 import react from '@vitejs/plugin-react';
 
+// https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
   build: {
     outDir: 'dist',
     rollupOptions: {
-      output: {
-        entryFileNames: '[name].js',
-        chunkFileNames: '[name].js',
-        assetFileNames: '[name].[ext]'
-      }
+      input: 'index.html'
     }
   },
   server: {
     mimeTypes: {
-      'application/javascript': ['js'],
-      'application/javascript': ['jsx']
+      'application/javascript': ['js', 'jsx']
     }
   }
 });
